2531a92cba88d38d6a868144f10e3ae3adb29da8
Dipesh63 <134762024+Dipesh63@users.noreply.github.com>
2024-11-20 19:51:12 +0600

    first commit

---
+++ cardgame02/cardgame02App.swift
@@ -0,0 +1,17 @@
+//
+//  cardgame02App.swift
+//  cardgame02
+//
+//  Created by Gaming Lab on 31/10/24.
+//
+
+import SwiftUI
+
+@main
+struct cardgame02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
